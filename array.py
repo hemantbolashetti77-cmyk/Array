053d4d2252fc3217e2ecc65a456e19b695e29a85
hemant17 <hemantbolashetti77@gamail.com>
2025-11-29 09:18:30 +0530

    Array

--- array.py
+++ array.py
@@ -7,8 +7,13 @@ else:
     print("No input provided – using default values")
     scores = [50, 60, 70, 80, 90]
 
+
 total = sum(scores)
 average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
 
 print("Sum of scores:", total)
 print("Average of scores:", average)
+print("Maximum score:", maximum)
+print("Minimum score:", minimum)
